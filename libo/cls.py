11771c144d731d9e177fa0eeaa13f63fd8b44f6f
@geo_inca <miq182@inlumine.ual.es>
2019-03-21 11:33:57 +0100

    mprimir log de APP
    [ticket: X]

---
+++ libo/cls.py
@@ -0,0 +1,4 @@
+import os
+# Terminal cleaning
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
---
+++ libo/doit.py
@@ -0,0 +1,5 @@
+import libo.logo
+import libo.cls
+def doit():
+    libo.cls.cls()
+    libo.logo.printlogo()
